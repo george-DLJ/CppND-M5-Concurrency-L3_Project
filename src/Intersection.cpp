2e93c5388a3415219aaab22a122d80318e44af7d
George-DLJ <38262125+george-DLJ@users.noreply.github.com>
2020-12-30 12:11:54 +0000

    feat: task L3.3 lock console outout as shared resourece
    Task L3.3 : In method Intersection::addVehicleToQueue and in Vehicle::drive() ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std::cout at the beginning and at the end of addVehicleToQueue the lock is not held

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -83,10 +83,13 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 // adds a new vehicle to the queue and returns once the vehicle is allowed to enter
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
-    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held. 
-
+    // L3.3 : Ensure that the text output locks the console as a shared 
+    //        resource. Use the mutex _mtxCout you have added to the base class TrafficObject 
+    //        in the previous task. Make sure that in between the two calls to std-cout at 
+    //        the beginning and at the end of addVehicleToQueue the lock is not held. 
+    TrafficObject::_mtxCout.lock();
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
-
+    TrafficObject::_mtxCout.unlock();
     // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
     std::future<void> ftrVehicleAllowedToEnter = prmsVehicleAllowedToEnter.get_future();
@@ -94,7 +97,9 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
+    TrafficObject::_mtxCout.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
+    TrafficObject::_mtxCout.unlock();
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
--- src/TrafficObject.h
+++ src/TrafficObject.h
@@ -34,10 +34,11 @@ protected:
     int _id;                          // every traffic object has its own unique id
     double _posX, _posY;              // vehicle position in pixels
     std::vector<std::thread> threads; // holds all threads that have been launched within this object
+    static std::mutex _mtxCout;  // TASK L3.2: add static mutex on class TrafficObject (Protected so it can be used by inherited clasess!)
 
 private:
     static int _idCnt; // global variable for counting object ids
-    static std::mutex _mtxCout;  // TASK L3.2: add static mutex on class TrafficObject
+
 };
 
 #endif
